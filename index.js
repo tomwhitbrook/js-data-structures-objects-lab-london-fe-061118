d98f8489d6d3577a0e0697193ade1b38ea7a9f02
Tom Whitbrook <tomwhitbrook@gmail.com>
2018-07-25 17:13:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution in this file!
+const driver = {
+ 
+}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+
+  let cloneDriver = {...driver}
+  
+  cloneDriver[key] = value
+
+return cloneDriver
+
+}
